c097dc63dfc7f78e927bbe650249e141690c5691
Sergio Brunacci <sergiodealmeidabrunacci@Sergios-MacBook-Pro.local>
2017-09-27 21:06:00 -0400

    Initial Commit

---
+++ Restructure-Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restructure-Map
+//
+//  Created by Sergio de Almeida Brunacci on 2017-09-27.
+//  Copyright © 2017 Sergio de Almeida Brunacci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
